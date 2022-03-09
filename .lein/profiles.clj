c89bea9892daf32ce39c3d4f49a6f8958a716b39
Alan <cloojure@gmail.com>
2022-03-08 21:18:37 -0800

    misc

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,10 +1,9 @@
 {
   :user {
     :plugins [
-      [com.jakemccrary/lein-test-refresh "0.24.1"]
+      [com.jakemccrary/lein-test-refresh "0.25.0"]
      ;[jonase/eastwood          "0.4.3"]
       [lein-ancient             "0.7.0"]
-      [lein-codox               "0.10.8"]
       [lein-exec                "0.3.7"]]
     :jvm-opts ["-Xms1g" "-Xmx4g" 
 ;              "-server"
