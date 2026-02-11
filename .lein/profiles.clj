64c1db403878293f0ad3200af1fab1de42e890d6
Alan Thompson <alan.thompson@walmart.com>
2026-02-11 14:57:07 -0800

    misc

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -7,7 +7,7 @@
 
   :user {
     :plugins [
-      [com.jakemccrary/lein-test-refresh "0.25.0"]
+      [com.jakemccrary/lein-test-refresh "0.26.0"]
      ;[jonase/eastwood          "0.4.3"]
       [lein-ancient             "0.7.0"]
       [lein-exec                "0.3.7"]]
